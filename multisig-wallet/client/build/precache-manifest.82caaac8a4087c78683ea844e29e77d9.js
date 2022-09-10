28cd9bc26ff0fc8fdd6f9a03329ebc795941c011
Yogesh <81234066+yogesh-js@users.noreply.github.com>
2022-09-10 13:18:56 +0530

    Add files via upload

---
+++ multisig-wallet/client/build/precache-manifest.82caaac8a4087c78683ea844e29e77d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e3b7b87223d3777be25a769e49b98b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ce6f4c8533b7ae89909",
+    "url": "/static/js/2.b926b2a1.chunk.js"
+  },
+  {
+    "revision": "421403cb0a1e353525f7444ed7bf2f24",
+    "url": "/static/js/2.b926b2a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3180c61feb895e1eda89",
+    "url": "/static/js/main.72730d35.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.72730d35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
